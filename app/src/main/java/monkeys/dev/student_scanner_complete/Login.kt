b54ac37b57dd6689dce5442d9529cc2a79cdc1dd
RigoUcol <bzamora5@ucol.mx>
2023-05-15 19:08:56 -0600

    Conexión general del proyecto con firebase

--- app/src/main/java/monkeys/dev/student_scanner_complete/Login.kt
+++ app/src/main/java/monkeys/dev/student_scanner_complete/Login.kt
@@ -6,12 +6,20 @@ import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+
+    //SE DECLARA EL OBJETO DE AUTENTICACION
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
+        //SE INICIALIZA EL AUTH
+        auth = Firebase.auth
         val iniciar_sesion = findViewById<Button>(R.id.btn_iniciar_sesion) as Button
 
         iniciar_sesion.setOnClickListener {
