df2d9735ff261770eac9118b8d4fc4a9e6be13a2
RigoUcol <bzamora5@ucol.mx>
2023-05-17 19:50:40 -0600

    Acceso y registro de usuarios nuevos implementados exitosamente; se requiere un boton de regresar en el registro de nuevos usuarios y un ¿Olvidaste tu contraseña? en el login

--- app/src/main/java/monkeys/dev/student_scanner_complete/Login.kt
+++ app/src/main/java/monkeys/dev/student_scanner_complete/Login.kt
@@ -3,20 +3,64 @@ package monkeys.dev.student_scanner_complete
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+
+    //-SE DECLARA EL OBJETO DE AUTENTICACION-//
+    private lateinit var firebaseAuth: FirebaseAuth
+    //private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        val iniciar_sesion = findViewById<Button>(R.id.btn_iniciar_sesion) as Button
+        //-Variables que con los datos que el usuario introduce-//
+        var user = findViewById<EditText>(R.id.etxt_correo_ucol) as EditText
+        var password = findViewById<EditText>(R.id.etxt_contraseña) as EditText
 
+        //-Variables para los botones-//
+        val iniciar_sesion = findViewById<Button>(R.id.btn_iniciar_sesion) as Button
+        val registrarse = findViewById<Button>(R.id.btn_registrarse) as Button
+
+        //-SE INICIALIZA EL AUTH-//
+        firebaseAuth = Firebase.auth
+
+        //-Boton de iniciar sesion-//
         iniciar_sesion.setOnClickListener {
-            val intent = Intent(this,Frame_Container::class.java)
-            startActivity(intent)
+            singIn(user.text.toString(), password.text.toString())
+        }
+
+        //-Boton de registro de nuevos usuarios-//
+        registrarse.setOnClickListener {
+            val nuevo = Intent(this, User_register::class.java)
+            startActivity(nuevo)
+        }
+
+    }
+
+    //-Función para evaluar el inicio de sesión-//
+    private fun singIn(email: String, password: String){
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){ task ->
+            if (task.isSuccessful){
+                val user = firebaseAuth.currentUser
+                Toast.makeText(baseContext,"Inicio de sesión exitoso", Toast.LENGTH_LONG).show()
+
+                val intento = Intent( this, Frame_Container::class.java)
+                startActivity(intento)
+            }else{
+                Toast.makeText(baseContext,"Error: Usuario y/o contraseña incorrectos", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
--- app/src/main/java/monkeys/dev/student_scanner_complete/User_register.kt
+++ app/src/main/java/monkeys/dev/student_scanner_complete/User_register.kt
@@ -1,11 +1,62 @@
 package monkeys.dev.student_scanner_complete
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class User_register : AppCompatActivity() {
+
+    //-SE DECLARA EL OBJETO DE AUTENTICACION-//
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_register)
+
+        //SE INICIALIZA EL AUTH
+        firebaseAuth = Firebase.auth
+
+        //-Variables que recopilan la nueva credencial-//
+        val nuevoemail = findViewById<EditText>(R.id.etxt_correo_ucol) as EditText
+        val nuevopassword = findViewById<EditText>(R.id.etxt_contraseña) as EditText
+
+        //-Variables de los botones-//
+        val registrarse = findViewById<Button>(R.id.btn_registrarse) as Button
+
+        //-Registro de nuevo usuario-//
+        registrarse.setOnClickListener {
+            createAccount(nuevoemail.text.toString(), nuevopassword.text.toString())
+        }
+    }
+
+    private fun createAccount(email: String, password: String){
+        //-Inicia función-//
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(baseContext,"Usuario creado exitosamente, ¡Inicia sesión!", Toast.LENGTH_LONG).show()
+                    val user = firebaseAuth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext,"Error, verifica tu conexión a internet", Toast.LENGTH_LONG).show()
+                    updateUI(null)
+                }
+            }
+        //-Fin de la función-//
+    }
+
+    private fun updateUI(user: FirebaseUser?){
+        //-Lo procesa como nuevo usuario a Firebase-//
     }
 }
